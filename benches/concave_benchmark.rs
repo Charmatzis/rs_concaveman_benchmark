58844f1bdb6d0ee4665e96cf9501f45f7d2ea9cd
Christos Charmatzis <christos.charmatzis@partners.xarvio.com>
2023-03-09 13:33:43 +0200

    pr

--- benches/concave_benchmark.rs
+++ benches/concave_benchmark.rs
@@ -33,12 +33,12 @@ fn concave(_n: u32) -> () {
 
     let pow_of_ten:i128 = i128::pow(10, _n);
     let raw_points: Vec<Point> = (0..pow_of_ten).map(|_v| rng.gen()).collect();
-    concaveman(&raw_points, None, None);
+    concaveman(&raw_points, Some(0.2), None);
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("create concave hull from 10^6 random points", |b| b.iter(|| concave(black_box(6))));
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(name=benches; config=Criterion::default().sample_size(10); targets=criterion_benchmark);
 criterion_main!(benches);
